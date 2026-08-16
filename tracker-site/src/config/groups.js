8b2934f57ecf62642a68b85555caf8c5d7370305
acrystalee-max <acrystalee@gmail.com>
2026-08-16 13:57:24 +0700

    Six groups: add tracker-site/src/config/groups.js

---
+++ tracker-site/src/config/groups.js
@@ -0,0 +1,13 @@
+export const GROUPS = Array.from({ length: 6 }, (_, index) => {
+  const number = index + 1
+  return {
+    id: `Gr${number}`,
+    name: `Группа ${number}`,
+    accountEmail: `group${number}@english-tracker.local`,
+  }
+})
+
+export function getRequestedGroup() {
+  const requested = new URLSearchParams(window.location.search).get('group')
+  return GROUPS.find((group) => group.id === requested) || GROUPS[0]
+}
